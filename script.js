ca4fbccf61965c97d0576f6b46137830d1185b3a
Olanrewaju Oluyomi <122275393+Cartier19@users.noreply.github.com>
2023-03-28 15:17:12 +0000

    Code in progress

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 var currentSlide = 1;
 
 // add an event listener to document.body here
+var btn = document.body
+btn.addEventListener('click', () => {
+  console.log('clicked')
+  console.log(currentSlide)
+  var slides = document.querySelector('.s-' + currentSlide)
+  slides.classList.add('hidden')
+  currentSlide = currentSlide + 1
+  // slides.classList.remove('hidden')
+  console.log(slides)
+})
